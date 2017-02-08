f2f55bc95c9efa20d36100355af5b240001531eb
Rico <richardthomaswhitfield@gmail.com>
2017-02-08 14:22:51 -0600

    passing chhose customer test and passing activate customer test. also customer and superuser module

---
+++ superuser.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+class Superuser():
+
+	def register_user(self,user):
+		pass
+
+	def user_is_registered(self,user):
+		return True
+
+	def activate_customer(self,user):
+		pass
+
+	def user_is_active(self,user):
+		return True
