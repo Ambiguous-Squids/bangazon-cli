d4ca95f21d7fb5d16011983d9f280d09d32f79cb
Ali Kimbrell <alexandrakimbrell@gmail.com>
2017-02-08 15:25:45 -0600

    WIP

---
+++ TestChooseActiveCustomer.py
@@ -0,0 +1,21 @@
+import unittest
+from .customer import Customer
+from .superuser import Superuser
+
+
+class TestChooseActiveCustomer(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(self):
+		self.albert = Customer("Albert", "Einstein","123 Atom Way",
+							"Apt. B2", "Nashville", "TN",
+							"32233", "615-555-555", "bigal@al.com")
+		self.superuser = Superuser()
+
+
+	def test_superuser_can_choose_active_customer(self):
+		
+		self.superuser.activate_customer(self.albert)
+
+if __name__ == '__main__':
+	unittest.main()
---
+++ customer.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+class Customer():
+
+	def __init__(self, first_name):
+		# , last_name, address_1,
+				# address_2, city, state, zip, phone_number, email):
+
+		self.first_name = first_name
+		# self.last_name = last_name
+		# self.address_1 = address_1
+		# self.address_2 = address_2
+		# self.city = city
+		# self.state = state
+		# self.zip = zip
+		# self.phone_number = phone_number
+		# self.email = email
+
+
