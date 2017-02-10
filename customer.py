b0424c1c340135ba4386c06bd833d74a1d64153a
Rico <richardthomaswhitfield@gmail.com>
2017-02-10 11:11:41 -0600

    inital testing and structure

--- customer.py
+++ customer.py
@@ -31,6 +31,35 @@ class Customer():
 		self.phone_number = phone_number
 		self.email = email
 
+	def get_full_name(self):
+	    return "{} {}".format(self.first_name, self.last_name)
+
+	def get_first_name(self):
+	    return str(self.first_name)
+
+	def get_last_name(self):
+	    return str(self.last_name)
+
+	def get_address_1(self):
+	    return str(self.address_1)
+
+	def get_address_2(self):
+	    return str(self.address_2)
+
+	def get_city(self):
+		return str(self.city)
+
+	def get_state(self):
+		return str(self.state)
+
+	def get_zip(self):
+		return str(self.state)
+
+	def get_phone_number(self):
+		return str(self.phone_number)
+
+	def get_email(self):
+	    return str(self.email)
 
 	def user_is_active(self,user):
 		return True
@@ -40,3 +69,25 @@ class Customer():
 
 	def deactivate_customer(self,user):
 		return False
+
+
+	def register_user_in_db(self,user):
+
+		with sqlite3.connect("bangazon.db") as b:
+		            cursor = b.cursor()
+
+		            cursor.execute("""
+		            INSERT INTO Customer VALUES (null, '{}', '{}', '{}', '{}')
+		            """.format(
+		                        get_first_name(), 
+		                        get_last_name(), 
+		                        get_address_1(), 
+		                        get_address_2(), 
+		                        get_city(), 
+		                        get_state(), 
+		                        get_zip(), 
+		                        get_phone_number(), 
+		                        get_email()))
+
+
+
--- tests/TestCreateCustomer.py
+++ tests/TestCreateCustomer.py
@@ -3,7 +3,7 @@ sys.path.append('../')
 
 import unittest
 from customer import Customer
-from superuser import Superuser
+# from superuser import Superuser
 
 class TestCreateCustomer(unittest.TestCase):
 
@@ -25,16 +25,17 @@ class TestCreateCustomer(unittest.TestCase):
 		self.albert = Customer("Albert", "Einstein","123 Atom Way",
 							"Apt. B2", "Nashville", "TN",
 							"32233", "615-555-555", "bigal@al.com")
-		self.superuser = Superuser()
+		# self.superuser = Superuser()
 
 
 
 	def test_superuser_can_create_customer_account(self):
 
 		self.assertIsInstance(self.albert, Customer)
+		self.register_user_in_db(self.albert)
 
-		self.superuser.register_user(self.albert)
-		self.assertTrue(self.superuser.user_is_registered(self.albert))
+		# self.superuser.register_user(self.albert)
+		# self.assertTrue(self.superuser.user_is_registered(self.albert))
 
 
 if __name__ == '__main__':
