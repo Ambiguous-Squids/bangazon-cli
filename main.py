51c47e6cdf4a48d5cab29de36a1368b1d17c686d
Rico <richardthomaswhitfield@gmail.com>
2017-02-13 13:16:05 -0600

    skeleton for main menu functions

--- main.py
+++ main.py
@@ -11,6 +11,59 @@ def main():
 	print("6. See product popularity \n")
 	print("z. Leave Bangazon! \n")
 
-	choice = int(input())
 
+	while True:
+		'''enables continuous interface until user exits'''
+		try:
+			'''controls error'''
+			choice = int(input())
+
+			if choice == 1:
+				createCustomer()
+				break
+
+			if choice == 2:
+				chooseCustomer()
+				break
+
+			if choice == 3:
+				createPayment()
+				break
+
+			if choice == 4:
+				addProduct()
+				break
+
+			if choice == 5:
+				completeOrder()
+				break
+
+			if choice == 6:
+				getProductPopularity()
+				break
+
+			elif choice == 7:
+				quit()
+				break
+
+			else:
+				print("Please enter a valid choice")
+		except ValueError:
+				print("Please enter a valid choice")
+	exit
+
+def createCustomer():
+	print("createCustomer")
+def chooseCustomer():
+	print("chooseCustomer")
+def createPayment():
+	print("createPayment")
+def addProduct():
+	print("addProduct")
+def completeOrder():
+	print("completeOrder")
+def getProductPopularity():
+	print("getProductPopularity")
+def quit():
+	exit
 main()
