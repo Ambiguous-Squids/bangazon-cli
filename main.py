9476cebe3987cb95d20b20a531b21bed613f00a2
Rico <richardthomaswhitfield@gmail.com>
2017-02-13 12:01:54 -0600

    inital setup of main menu function

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+
+	print("\n""******************************************************* \n"
+		"** Welcome to Bangazon! Command Line Ordering System ** \n"
+		"*******************************************************")
+	print("1. Create a customer account \n")
+	print("2. Choose active customer \n")
+	print("3. Create a payment option \n")
+	print("4. Add product to shopping cart \n")
+	print("5. Complete an order \n")
+	print("6. See product popularity \n")
+	print("z. Leave Bangazon! \n")
+
+	choice = int(input())
+
+main()
