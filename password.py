08adaa178d37fc880a438c2a135a41a281f62f9e
asimonia <alex.simonian@gmail.com>
2017-02-18 17:21:16 -0600

    Added a password module

---
+++ password.py
@@ -0,0 +1,38 @@
+'''
+Module for password encryption using the bcrypt library
+See: https://pypi.python.org/pypi/bcrypt/3.1.0
+'''
+
+import bcrypt
+
+
+class Password:
+
+	'''
+		Purpose:
+			This class handles encryption process of the password
+
+		Methods:
+
+
+		Author:
+			@asimonia
+
+	'''
+
+	def __init__(self, password):
+		"""Takes a password as string, specified by user and initializes a hashed
+		password as bytes for database storage.
+		"""
+
+		# convert the string into bytes
+		self.password = str.encode(password)
+
+		# Hash a password for the first time, with a randomly-generated salt
+		self.hashed = bcrypt.hashpw(self.password, bcrypt.gensalt())
+
+	def get_hashed_password(self):
+		return self.hashed
+
+
+
